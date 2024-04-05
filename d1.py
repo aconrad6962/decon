7062669410ab5311e6cd9f624c6988ccbafb624c
aconrad6962 <53485665+aconrad6962@users.noreply.github.com>
2024-04-04 15:24:21 -1000

    Modified for latest image from Ashley and generalized dimensions for centering, etc

--- d1.py
+++ d1.py
@@ -4,6 +4,8 @@
 #
 #  ARC - 15nov2018
 #
+#  30mar2024:  adapting to Io/Live/SV instead of Vesta/GMT/Gem
+#
 import numpy as np
 import matplotlib
 from astropy.io import fits
@@ -22,21 +24,25 @@ os.system( "rm -f ./foo.fits" )
 #
 #  Constants
 #
-eeWid = 15     # edge effect width
-yPadTop  = 5000000  # to center plots
-yPadBot  = 10000
-lbtiErr  = 2000000
-jwstErr  = 500000
-fname = './FC21A0002835_11191050436F4N.FIT'
 width    = 1024
-
 
 nr=2
 nc=2
 fig, ax = plt.subplots(nrows=nr,ncols=nc)
 
-# loh - use:  img = iio.imread("g4g.png")
-im = fits.getdata( fname )
+#imJpg = iio.imread("PIA00740.jpg")
+imPng = iio.imread("SHARK_Nov_2023_264W_3.3N_0rot.png")
+print( "png shape", imPng.shape )
+#imoffx = 240
+#imoffy = 300
+midp = width/2
+imoffx = int(midp-(imPng.shape[0]/2))
+imoffy = int(midp-(imPng.shape[1]/2))
+
+im = np.zeros([width,width])
+im[imoffx:imoffx+imPng.shape[0],
+   imoffy:imoffy+imPng.shape[1]] = \
+     imPng[0:imPng.shape[0],0:imPng.shape[1],0]
 
 for i in range(0,nr):
   for j in range(0,nc):
@@ -44,66 +50,63 @@ for i in range(0,nr):
     ax[i,j].set_yticks([])
 
 #
-# Devise JWST or LBTI psf from 1st pncpls: 
+# Devise dlim of psf (in pixels):
 #
-#   0. (Guess at subtended arcseconds of sci foV = foVst)
-#   1. plateScale = imageWidth /foVst  (make image square first if nccssry)
-#   2. Define dlim = 4 mas for GMT GMagAO-X (at 0.5 microns from j males talk)
-#   3. Gen the BMT psf (??)
-#       a. Find any airy patern = apat
-#       b. psfPixWid = plateScale * dlim
+#   0. Estimate subtended arcseconds of sci FoV
+#   1. plateScale = imageWidth /FoV  (make image square first if nccssry)
+#   2. Define the 3 dlims
+#   3. psfPixWid = plateScale * dlim (3 times)
+#   4. Gen the psf using airyDisk2Dkernel
+#   5. Convolve using convolve_fft
 #
-#   Vesta is about 0.7 arcsec and 160 out of 1024 pix
-#   so FoV is 1024/160*.7 = 4.5 arcsec
-#
-#
-foVst         = 4500   # milliarsec
-gmtDlim       = 4    # for GMT GMagAO-X (at 0.5 microns from j males talk)
-gemDlim       = gmtDlim * 2.2/0.5 * 26 / 8  # Gemini at K
-gemDlim_R     = gmtDlim           * 26 / 8  # Gemini at K
+FoV           = 1000   # milliarsec
+livDlim       = 5      # scaling from 4 for GMT GMagAO-X
+                       # at 0.5 microns from j males talk
+svDlim        = 15
+fizDlim       = livDlim * 2.2 / 0.5
 
-plateScale = im.shape[0] / foVst   #  chk if square
+plateScale = im.shape[0] / FoV   #  chk if square
 
-gmtPsfPixWid  = plateScale * gmtDlim
-radius = gmtPsfPixWid       # Size in pixels of Airy pattern (to first null)
+livPsfPixWid  = plateScale * livDlim
+radius = livPsfPixWid       # Size in pixels of Airy pattern (to first null)
 psf = AiryDisk2DKernel(radius)
-gmtRim = convolve_fft(im, psf )
+livRim = convolve_fft(im, psf )
 
-gemPsfPixWid  = plateScale * gemDlim
-radius = gemPsfPixWid       # Size in pixels of Airy pattern (to first null)
+svPsfPixWid  = plateScale * svDlim
+radius = svPsfPixWid       # Size in pixels of Airy pattern (to first null)
 psf = AiryDisk2DKernel(radius)
-gemKim = convolve_fft(im, psf )
+svRim = convolve_fft(im, psf )
 
-gemPsfPixWid_R  = plateScale * gemDlim_R
-radius = gemPsfPixWid_R       # Size in pixels of Airy pattern (to first null)
+fizPsfPixWid  = plateScale * fizDlim
+radius = fizPsfPixWid       # Size in pixels of Airy pattern (to first null)
 psf = AiryDisk2DKernel(radius)
-gemRim = convolve_fft(im, psf )
+fizRim = convolve_fft(im, psf )
 
 print( "plateScale = " + "%lf" % plateScale + " pixel/mas" )
-print( "gmtDlim = " + "%lf" % gmtDlim )
-print( "gmtPsfPixWid = " + "%lf" % gmtPsfPixWid + " = plateScale * gmtDlim" )
+print( "livDlim = " + "%lf" % livDlim )
+print( "livPsfPixWid = " + "%lf" % livPsfPixWid + " = plateScale * livDlim" )
 
 #
 #  Plots start here
 #
-midp = width/2
-yf=-40    #  yfudge
-xf=+10
-zwid = 100
+#yf=-40    #  yfudge
+#xf=+10
+yf=0    #  yfudge
+xf=0
+zwid = 400
 
 zim1 = np.zeros([zwid*2,zwid*2])
-# print( "err val = ", midp-zwid+yf )
-zim1 = gmtRim[int(midp-zwid+yf):int(midp+zwid-1+yf), \
+zim1 = livRim[int(midp-zwid+yf):int(midp+zwid-1+yf), \
               int(midp-zwid+xf):int(midp+zwid-1+xf)]
 ax[0,0].imshow( zim1, cmap='gray' )
-ax[0,0].text(0.5,-0.08, "(GMT at 0.5 microns)", size=12, ha="center",
+ax[0,0].text(0.5,-0.08, "LIVE at 0.5 microns", size=12, ha="center",
         transform=ax[0,0].transAxes)
 
 zim2 = np.zeros([zwid*2,zwid*2])
-zim2 = gemKim[int(midp-zwid+yf):int(midp+zwid-1+yf),
+zim2 = fizRim[int(midp-zwid+yf):int(midp+zwid-1+yf),
               int(midp-zwid+xf):int(midp+zwid-1+xf)]
 ax[1,1].imshow( zim2, cmap='gray' )
-ax[1,1].text(0.5,-0.08, "(Gem at 2.2 microns)", size=12, ha="center",
+ax[1,1].text(0.5,-0.08, "LBTI Fizeau at 2.2 microns", size=12, ha="center",
         transform=ax[1,1].transAxes)
 
 zim3 = np.zeros([zwid*2,zwid*2])
@@ -111,15 +114,14 @@ zim3 = np.zeros([zwid*2,zwid*2])
 zim3 = im[int(midp-zwid+yf):int(midp+zwid-1+yf),
           int(midp-zwid+xf):int(midp+zwid-1+xf)]
 ax[0,1].imshow( zim3, cmap='gray' )
-ax[0,1].text(0.5,-0.08, "Spacecraft image)", size=12, ha="center",
+ax[0,1].text(0.5,-0.08, "Spacecraft image", size=12, ha="center",
         transform=ax[0,1].transAxes)
 
 zim4 = np.zeros([zwid*2,zwid*2])
-# zim4 = gemRim[midp-zwid+yf:midp+zwid-1+yf,midp-zwid+xf:midp+zwid-1+xf]
-zim4 = gemRim[int(midp-zwid+yf):int(midp+zwid-1+yf),
+zim4 = svRim[int(midp-zwid+yf):int(midp+zwid-1+yf),
               int(midp-zwid+xf):int(midp+zwid-1+xf)]
 ax[1,0].imshow( zim4, cmap='gray' )
-ax[1,0].text(0.5,-0.08, "Gem at 0.5 microns)", size=12, ha="center",
+ax[1,0].text(0.5,-0.08, "SHARK-VIS at 0.5 microns", size=12, ha="center",
         transform=ax[1,0].transAxes)
 
 #
